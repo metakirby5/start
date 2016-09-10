a5bec8c1faf7f5b2746f93ebb2b2ce847d284dca
Ethan Chan <metakirby5@gmail.com>
2016-09-10 01:18:53 -0700

    static -> dist

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,7 @@
+port module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.App as App
 
 main = text "Hello, World!"
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
+// CSS
 require('skeleton/css/normalize.css');
 require('skeleton/css/skeleton.css');
 require('./style');
 
-var elm = require('./Main').Main.fullscreen();
+var main = require('./Main').Main.fullscreen();
+var yaml = require('js-yaml');
