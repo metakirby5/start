ff451af86580d6e74fbed446beb6a9c51388c5f6
Ethan Chan <metakirby5@gmail.com>
2016-09-10 02:51:35 -0700

    do beginner program first

--- src/Main.elm
+++ src/Main.elm
@@ -2,17 +2,47 @@ port module Main exposing (..)
 
 import Debug
 import Html exposing (..)
-import Html.Attributes exposing (..)
 import Html.App as App
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
 
-main = text "Hello, World"
+main = App.beginnerProgram
+  { model = model
+  , view = view
+  , update = update }
+
+type alias Model = Int
+model : Model
+model = 0
+
+type Msg = Increment | Decrement | Reset
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment -> model + 1
+    Decrement -> model - 1
+    Reset -> 0
+
+view : Model -> Html Msg
+view model =
+  div [style [("padding", "4%")]]
+    [ div [class "row"]
+      [ div [class "twelve columns", style [("text-align", "center")]]
+        [text (toString model)] ]
+    , div [class "row", style [("margin-top", "4%")]]
+      [ button [class "one-third column", onClick Decrement] [text "-"]
+      , button [class "one-third column", onClick Reset] [text "Reset"]
+      , button [class "one-third column", onClick Increment] [text "+"]
+      ]
+    ]
 
 -- main : Program (Maybe Model)
 -- main = App.programWithFlags
 --   { init          = init
 --   , view          = view
 --   , update        = update
---   , subscriptions = \_ -> Sub.none }
+--   , subscriptions = \_ -> Sub.none
+--   }
 
 -- -- Models
 -- type alias Model = { options : Options, tree: Tree }
@@ -24,7 +54,8 @@ main = text "Hello, World"
 --     , name    : String
 --     , link    : String
 --     , search  : String
---     , entries : Maybe (List Tree) }
+--     , entries : Maybe (List Tree)
+--     }
 
 -- -- Updates
 -- type Msg
