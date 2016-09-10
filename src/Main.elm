61c447f4c1a0c6a281b0fd61a7a23179c02da83b
Ethan Chan <metakirby5@gmail.com>
2016-09-10 02:09:17 -0700

    got yaml in

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,41 @@
 port module Main exposing (..)
 
+import Debug
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App as App
 
-main = text "Hello, World!"
+main = text "Hello, World"
+
+-- main : Program (Maybe Model)
+-- main = App.programWithFlags
+--   { init          = init
+--   , view          = view
+--   , update        = update
+--   , subscriptions = \_ -> Sub.none }
+
+-- -- Models
+-- type alias Model = { options : Options, tree: Tree }
+-- type alias Options = { startSearching : Bool }
+-- type Tree
+--   = Root (List Tree)
+--   | Node
+--     { key     : String
+--     , name    : String
+--     , link    : String
+--     , search  : String
+--     , entries : Maybe (List Tree) }
+
+-- -- Updates
+-- type Msg
+--   = Nil
+
+-- update : Msg -> Model -> (Model, Cmd Msg)
+-- update msg model =
+--   case msg of
+--     Nil -> (model, Cmd.none)
+
+-- -- Views
+-- view : Model -> Html Msg
+-- view model =
+--   div [] [ text "Hello, World" ]
--- src/main.js
+++ src/main.js
@@ -7,3 +7,16 @@ require('./style');
 
 var main = require('./Main').Main.fullscreen();
 var yaml = require('js-yaml');
+var $ = require('jquery');
+
+var CONFIG = '/static/config.yaml';
+
+$.ajax({
+  url: CONFIG,
+  success: function(data) {
+    console.log(yaml.safeLoad(data));
+  },
+  error: function() {
+    alert('Could not fetch ' + CONFIG + '!');
+  },
+});
